5cbcdfc4a23a688046af33d6fe8311bd9f3a754d
KRAlphacar <cgiasdf@gmail.com>
2025-12-07 18:13:28 -0600

    code

--- led.s
+++ led.s
@@ -5,8 +5,7 @@
 #include "avr/io.h"
 ;------------------------
 .global start
-.global led
-.global button          ; Make sure button is global if called from C
+.global button          
 .equ BUTTON, PD2        ; define BUTTON pin
 ;------------------------
 start:
@@ -23,30 +22,28 @@ button:
     IN    R22, PIND             ; read port D into R22
     ANDI  R22, (1<<BUTTON)      ; Mask everything except the button bit
     CPI   R22, (1<<BUTTON)      ; compare with pushed button value
-    BREQ  ledOFF                ; if equal (High), branch to ledOFF
+    BREQ  return                ; if equal (High), branch to return
     
-    LDI   R24, 0x01             ; load 1 into R24
     RCALL led                   ; call led subroutine 
-    RET                         ; return to loop() function
+    RCALL ledOFF                ; call ledOFF subroutine
+    RET                         ; return
+
+return:
+    RET                         ; return
 
 led:
-    CPI   R24, 0x00           ; value in R24 passed by caller compared with 0
-    BREQ  ledOFF              ; jump (branch) if equal to subroutine ledOFF
     SBI   PORTB, 4            ; set D12 to high
     SBI   PORTB, 5            ; set D13 to high
     RCALL myDelay
-    RET                       ; return to loop() function
+    RET                       ; return
 
 ledOFF:
-    SBI   PORTB, 4            ; set D12 to high
-    SBI   PORTB, 5            ; set D13 to high
-    RCALL myDelay
     CBI   PORTB, 4            ; set D12 to low
     CBI   PORTB, 5            ; set D13 to low
     RCALL myDelay
-    RET                       ; return to loop() function
+    RET                       ; return 
 
-.equ  delayVal, 1000         ; Note: This value results in ~25ms delay, not 1000ms
+.equ  delayVal, 1000          ; delay value 
 
 myDelay:
     LDI   R20, 100            ; initial count value for outer loop
